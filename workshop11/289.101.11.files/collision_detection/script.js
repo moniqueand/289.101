44fd086e0cb263b9b99d61ce2db5a73dae69266a
Andrews <16026735@wsom-072790.massey.ac.nz>
2018-05-22 16:04:06 +1200

    gsdrgf

---
+++ workshop11/289.101.11.files/collision_detection/script.js
@@ -0,0 +1,83 @@
+/*
+
+Begin by following the steps in the workshop notes.
+
+Next, add the following features:
+- the gap goes red when the puck is over it
+- the puck 'teleports' to the opposite boundary when it crosses one (think asteroids/snake)
+- create an item that makes the puck bigger
+- create an item that makes the puck smaller
+
+*/
+
+var puck_x = 210;
+var puck_y = 210;
+
+var puck = document.getElementById('puck');
+var speed = 10;
+var gap = document.getElementById('gap');
+
+function logCoordinates(){
+    console.log('x='+ puck_x + 'y='+ puck_y);
+}
+
+function checkCollision(){
+    if (puck_x >= 250 && puck_x < 350 && puck_y >= 150 && puck_y < 250 ){
+     gap.style.backgroundColor = 'red';   
+    }
+    
+    else{
+        gap.style.backgroundColor = 'white';
+    }
+}
+
+function teleportright(){
+    if (puck_x > 590){
+        puck_x = 0; 
+        puck.style.left = puck_x + 'px';
+    }
+}
+
+function teleportleft(){
+    if (puck_x <= 0){
+        puck_x = 600;
+        puck.style.right = puck_x + 'px';
+    }
+}
+
+puck.style.left = puck_x + 'px';
+puck.style.top = puck_y + 'px';
+
+document.getElementById('left').addEventListener('click', function(){
+    puck_x -= speed;
+   puck.style.left = puck_x + 'px';
+   logCoordinates();
+   checkCollision();
+    teleportleft();
+});
+
+document.getElementById('right').addEventListener('click', function(){
+    puck_x += speed;
+   puck.style.left = puck_x + 'px';
+   logCoordinates();
+   checkCollision();
+   teleportright();
+   
+});
+
+
+document.getElementById('up').addEventListener('click', function(){
+    puck_y -= speed;
+   puck.style.top = puck_y + 'px';
+   logCoordinates(); 
+   checkCollision();
+   
+});
+
+document.getElementById('down').addEventListener('click', function(){
+    puck_y += speed;
+   puck.style.top = puck_y + 'px';
+   logCoordinates(); 
+   checkCollision();
+
+});
